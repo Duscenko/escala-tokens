8a7de41f8088cea3ecb85f4b6c76a30a36d867df
Cesar Durango <duscenko@gmail.com>
2026-08-28 05:30:58 +0200

    chore: bundle updated Figma plugin

--- src/lib/pluginVersion.ts
+++ src/lib/pluginVersion.ts
@@ -10,4 +10,4 @@
 // (`pluginBuildSeen` in the store) against this to surface an "Update available"
 // hint on the Sync hub's "Download plugin" row.
 export const PLUGIN_VERSION = '0.2.0'
-export const PLUGIN_BUILD = 'aaeaf6369963'
+export const PLUGIN_BUILD = '65a28a395365'
